5c1bf02a843b4514694f9d64285e203287740aa1
Florian von Bock <f@vonbock.info>
2013-10-29 22:40:04 +0900

    extended version of binary.ReadVarint that returns byte length too

---
+++ util/varint.go
@@ -0,0 +1,36 @@
+package tris
+
+import (
+	"errors"
+	"io"
+)
+
+var overflow = errors.New("tris.reply: varint overflows a 64-bit integer")
+
+func ReadUvarint(r io.ByteReader) (uint64, int, error) {
+	var x uint64
+	var s uint
+	for i := 1; ; i++ {
+		b, err := r.ReadByte()
+		if err != nil {
+			return x, 0, err
+		}
+		if b < 0x80 {
+			if i > 9 || i == 9 && b > 1 {
+				return x, i, overflow
+			}
+			return x | uint64(b)<<s, i, nil
+		}
+		x |= uint64(b&0x7f) << s
+		s += 7
+	}
+}
+
+func ReadVarint(r io.ByteReader) (int64, int, error) {
+	ux, i, err := ReadUvarint(r) // ok to continue in presence of error
+	x := int64(ux >> 1)
+	if ux&1 != 0 {
+		x = ^x
+	}
+	return x, i, err
+}
