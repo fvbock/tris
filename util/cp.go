b92f4f47a89a8a061c3355e1dfb0b5a4860bb5e2
Florian von Bock <f@vonbock.info>
2013-12-04 00:30:58 +0900

    add stat utility function

--- util/cp.go
+++ util/cp.go
@@ -1,11 +1,12 @@
 package tris
 
 import (
+	// "fmt"
 	"io"
 	"os"
 )
 
-func CopyFile(dst, src string) error {
+func CopyFile(src string, dst string) error {
 	s, err := os.Open(src)
 	if err != nil {
 		return err
@@ -20,4 +21,16 @@ func CopyFile(dst, src string) error {
 		return err
 	}
 	return d.Close()
+}
+
+func PathExists(path string) (bool, error) {
+	_, err := os.Stat(path)
+	// fmt.Println("StatInfo:", statInfo)
+	if err == nil {
+		return true, nil
+	}
+	if os.IsNotExist(err) {
+		return false, nil
+	}
+	return false, err
 }
